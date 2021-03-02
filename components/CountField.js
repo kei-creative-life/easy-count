21858e53b6650165fb945328a1fd548f1776f7aa
KEI <68884990+kei1992@users.noreply.github.com>
2021-03-02 16:22:45 +0900

    コードの抽出

--- components/CountField.js
+++ components/CountField.js
@@ -1,10 +1,51 @@
 import { useState, useContext } from "react";
 import InputContext from "../contexts/InputContext";
 
-export default function CountField() {
-  const [text, setText] = useContext(InputContext);
+export default function CountField({ text }) {
   const [num, setNum] = useContext(InputContext);
   const [price, setPrice] = useState(0);
+  const [codePrice, setCodePrice] = useState(0);
+  const fixH2Title = (t) => {
+    if (t.match(/^## .+$/gm)) {
+      return t
+        .replace(/^## /gm, "")
+        .replace(/[ +　+]/gm, "")
+        .replace(/\r?\n/gm, "").length;
+      // if (t.match(/^.*### .+/gm)) {
+      //   return t
+      //     .replace(/(###+) .*/gm, "")
+      //     .replace(/^## /gm, "")
+      //     .replace(/[ +　+]/gm, "")
+      //     .replace(/\r?\n/gm, "").length;
+      // } else {
+      // }
+    }
+  };
+  const fixH3Title = (t) => {
+    if (t.match(/^### .+/gm)) {
+      return t
+        .replace(/^### /gm, "")
+        .replace(/[ +　+]/gm, "")
+        .replace(/\r?\n/gm, "");
+      // if (t.match(/^.*## .+$/gm)) {
+      //   return t
+      //     .replace(/^### /gm, "")
+      //     .replace(/^.*## .+$/gm, "")
+      //     .replace(/[ +　+]/gm, "")
+      //     .replace(/\r?\n/gm, "");
+      // } else {
+      // }
+    }
+  };
+
+  const countCode = (t) => {
+    if (t.match(/^```\w+\n.+\n(.*\n)*```$/gm)) {
+      return t
+        .replace(/(```\w+|```)/gm, "")
+        .replace(/[ +　+]/gm, "")
+        .replace(/\r?\n/gm, "").length;
+    }
+  };
 
   return (
     <div className="w-full">
@@ -23,9 +64,39 @@ export default function CountField() {
         <span>円/1文字</span>
       </div>
       <div>
+        <label htmlFor="unit-price">文字単価(コード)：</label>
+        <input
+          id="unit-code-price"
+          type="number"
+          placeholder="〇〇円/文字"
+          onChange={(e) => setCodePrice(e.target.value)}
+        />
+        <span>円/1文字</span>
+      </div>
+      <div>
         <span>合計金額：</span>
-        {text * price}
+        {num * price}
         <span>円</span>
+      </div>
+      <div>
+        <span>コード文字数：</span>
+        {countCode(text)}
+      </div>
+      <div>
+        <div>
+          <span># 見出し1：</span>
+        </div>
+        <div>
+          <span>## 見出し2：</span>
+          {fixH2Title(text)}
+        </div>
+        <div>
+          <span>### 見出し3：</span>
+          {fixH3Title(text)}
+        </div>
+        <div>
+          <span>#### 見出し4：</span>
+        </div>
       </div>
     </div>
   );
--- components/Form.js
+++ components/Form.js
@@ -1,17 +1,29 @@
 import { useState, useContext } from "react";
 import InputContext from "../contexts/InputContext";
 
-export default function Form() {
-  const [text, setText] = useState("");
+export default function Form({ text, setText }) {
   const [num, setNum] = useContext(InputContext);
+  const removeSpace = (event) => {
+    let trimText = event.target.value.trim();
+    let textResult = trimText.replace(/[ +\t]+/gm, "");
+    let removeNewLine = textResult.replace(/\r?\n/g, "");
+    setNum(removeNewLine.length);
+  };
   return (
     <div className="w-full p-3">
-      <label htmlFor="countForm">入力フォーム</label>
+      <label htmlFor="countForm">通常文字</label>
       <textarea
         id="countForm"
-        className="w-full bg-blue-100 h-full p-3"
+        className="w-full bg-blue-100 h-1/2 p-3"
         onKeyUp={(e) => setText(e.target.value)}
-        onChange={(e) => setNum(e.target.value.length)}
+        onChange={(e) => removeSpace(e)}
+      />
+      <label htmlFor="countCode">コード</label>
+      <textarea
+        id="countCode"
+        className="w-full bg-blue-100 h-1/2 p-3"
+        onKeyUp={(e) => setText(e.target.value)}
+        onChange={(e) => removeSpace(e)}
       />
     </div>
   );
